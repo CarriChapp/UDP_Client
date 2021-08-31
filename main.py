d2c26649fe831f70d9f3e6eff434de263fbe86dc
d4rkp0rt <andy@d4rkp0rt.co.uk>
2021-08-31 13:28:13 +0100

    Base code for the UDP client

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 """ A simple TCP client in Python """
 import socket
 
-target_host = "www.google.com"
+target_host = "127.0.0.1"
 target_port = 80
 
 # create a socket object
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# connect the client
-client.connect((target_host, target_port))
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # send some data
 message = 'GET / HTTP:/1.1\r\nHost: google.com\r\n\r\n'
-client.send(message.encode())
-response = client.recv(4096)
+client.sendto(message.encode(), (target_host, target_port))
 
-print(response)
+# receive some data
+data, addr = client.recvfrom(4096)
+
+print(data)
 
